823f477762c563178dcf2483ba01d98c19e87a1f
Raven <KindaIntellectual@users.noreply.github.com>
2017-02-16 10:18:51 -0500

    index.html -> index.php

---
+++ RPWPTheme/index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>RavenPitts.me </title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    <!-- CSS -->
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/normalize.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/master/devicon.min.css">
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css'>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="loaded">        
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!--<a class="navbar-brand" href="#">RavenPitts.me</a>-->
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#slide-1">Home</a></li>
+                    <li><a href="#slide-2">Projects</a></li>
+                    <li><a href="#slide-3">Skills</a></li>
+                    <li><a href="#slide-4">Contact</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </nav>
+    <main>
+    <section id="slide-1" class="homeSlide">
+            <div class="bcg"
+                data-center="background-position: 50% 0px;"
+                data-top-bottom="background-position: 50% -100px;"
+                data-anchor-target="#slide-1">
+                <div class="hsContainer">
+                    <div class="hsContent"
+                        data-center="bottom: 200px; opacity: 1"
+                        data-top="bottom: 200px; opacity: 0"
+                        data-anchor-target="#slide-1 p">
+        <h2><center>RavenPitts.me</center></h2>
+        <p class="lead">Hi! My name is Raven Pitts, and I'm an aspiring Front-End Web Developer with an emphasis on Javascript. Currently I'm learning to conquer HTML + CSS + Js via self-learning and hands-on implementation i.e. this Portfolio.</p>
+        <p class="skills">I have experience in design, html, css, js, as well as wordpress. While learning/working in wordpress I also had some experience with php and mySQL, at least enough to fix wordpress.</p>
+            </div>
+                    </div>
+                </div>
+        </section>
+    <section id="slide-2" class="homeSlide">
+            <div class="bcg"
+                 data-0="background-color:rgb(1,27,59);"
+        data-top="background-color:(0,0,0);"
+        data-anchor-target="#slide-2">
+                <div class="hsContainer">
+                    <div class="hsContent"
+                data-top="opacity: 1"
+                data--50-bottom="opacity: 1"
+                data-center-top="opacity: 0"
+                data--100-bottom="opacity: 0"
+                data-anchor-target="#slide-2">
+                        <div class="portfolio">
+            <h1>Projects</h1>
+			<img class="img-thumbnail" src="https://s5.postimg.org/4nljkfl6v/cfmp_2010.png" alt="Cell Phone Case Wordpress Amazon eCommerce Website" />			
+            <img class="img-thumbnail" src="https://s5.postimg.org/6r9skv1mv/tgn_2014.png" alt="Wordpress Tech Blog" />
+			<img class="img-thumbnail" src="https://s5.postimg.org/4v4l376rr/aiu_2012.png" alt="Apple iPhone Unlock Wordpress eCommerce Website" />
+			<img class="img-thumbnail" src="https://s5.postimg.org/x9a0n2ubr/aiu_2013.png" alt="Apple iPhone Unlock eCommerce Website Redesign" />
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <section id="slide-3" class="homeSlide">
+            <div class="bcg"
+                 data-center="background-position: 50% 0px;"
+        data-top-bottom="background-position: 50% -100px;"
+        data-bottom-top="background-position: 50% 100px;"
+        data-anchor-target="#slide-3">
+                <div class="hsContainer">
+                    <div class="hsContent"
+                         data--350-bottom="opacity: 0;"
+                data-center="opacity: 1"
+                data-center-top="opacity: 0"
+                data--200-top="opacity: 0"
+                data-anchor-target="#slide-3">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-xs-6 col-md-6"> 
+                                    <h1>Skills</h1><hr/>
+                                    <i class="devicon-html5-plain colored"></i>
+                                    <i class="devicon-css3-plain colored"></i><br/>
+                                    <i class="devicon-javascript-plain colored"></i><br/>
+                                    <i class="devicon-wordpress-plain"></i>
+
+                                </div>
+    <div class="col-xs-6 col-md-6"> 
+        <p class="skills">The "magic" behind websites has always fascinated me, like almost every other millenial I started out making templates for MySpace, eventually I dove deeper into web development and have built several websites using WordPress, as well as standalone HTML/CSS/JS websites.</p>
+        <br/>
+        <p class="skills">The links below are a few examples of what I have learned and code that I have written. </p>
+        <a href="https://www.codewars.com/users/KindaIntellectual/completed">CodeWars</a>
+        <a href="https://www.codecademy.com/Kindaintellectual">Codecademy</a>
+        <a href="https://www.freecodecamp.com/kindaintellectual">Free Code Camp</a>
+        <a href="https://github.com/KindaIntellectual">GitHub</a>
+                    
+                                </div>
+  </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <section id="slide-4" class="homeSlide">
+            <div class="bcg"
+                 data-center="background-position: 50% 0px;"
+        data-top-bottom="background-position: 50% -100px;"
+        data-bottom-top="background-position: 50% 100px;"
+        data-anchor-target="#slide-4">
+                <div class="hsContainer">
+                    <div class="hsContent"
+                         data-bottom-top="opacity: 0"
+                data-25p-top="opacity: 0"
+                data-top="opacity: 1" 
+                data-anchor-target="#slide-4">
+                        <h2> Contact Me</h2>
+                        <p class="skills">Sugestions, Feedback, or Questions?</p>
+                        <span class="fa-stack fa-lg">
+  			<i class="fa fa-circle fa-stack-2x"></i>
+  			<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+			</span>
+			<span class="fa-stack fa-lg">
+  			<i class="fa fa-circle fa-stack-2x"></i>
+  			<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+			</span>
+			<span class="fa-stack fa-lg">
+  			<i class="fa fa-circle fa-stack-2x"></i>
+  			<i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+			</span>
+			<span class="fa-stack fa-lg">
+  			<i class="fa fa-circle fa-stack-2x"></i>
+  			<i class="fa fa-github fa-stack-1x fa-inverse"></i>
+			</span>
+			<span class="fa-stack fa-lg">
+  			<i class="fa fa-circle fa-stack-2x"></i>
+  			<i class="fa fa-google-plus fa-stack-1x fa-inverse"></i>
+			</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <!-- /.container -->
+    </main>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/skrollr.js"></script>
+    <script src="js/imagesloaded.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
